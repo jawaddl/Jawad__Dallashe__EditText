ab0e3741fe14a0e953e202ebfc06bc8771f21d91
DESKTOP-FIA3LA0 <jawaddl2007@Gmail.com>
2024-02-05 18:25:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jawad_dallashe_edittext/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jawad_dallashe_edittext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
